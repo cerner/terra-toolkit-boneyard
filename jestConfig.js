bced6fd3779a7bdf1ed3984445ec93757cf71aec
Sireesha Nekkanti <nekkanti.sirisha1@gmail.com>
2020-04-15 11:33:07 -0600

    test results in result.txt but not as expected

--- jestConfig.js
+++ jestConfig.js
@@ -1,5 +1,6 @@
 module.exports = {
   collectCoverage: true,
+  verbose: true,
   collectCoverageFrom: [
     'config/**/*.js',
     'scripts/aggregate-themes/**/*.js',
@@ -14,6 +15,10 @@ module.exports = {
   ],
   testMatch: [
     '**/jest/**/(*.)(spec|test).js?(x)',
+  ],
+  reporters: [
+    'default',
+    '<rootDir>/src/jest/JestFileReporter.js',
   ],
   roots: [process.cwd()],
   testURL: 'http://localhost',
--- src/jest/JestFileReporter.js
+++ src/jest/JestFileReporter.js
@@ -1,14 +1,19 @@
-const VerboseReporter = require('@jest/reporters/build/verbose_reporter');
+const VerboseReporter = require('@jest/reporters/build/verbose_reporter').default;
+
+const fs = require('fs');
+// const path = require('path');
+
+// const filePath = path.resolve(__dirname, './result.txt');
 
 class JestFileReporter extends VerboseReporter {
+  // eslint-disable-next-line no-useless-constructor
   constructor(globalConfig) {
     super(globalConfig);
   }
 
   // eslint-disable-next-line class-methods-use-this
   log(message) {
-    console.log(message);
+    fs.appendFile('./result.txt', `${message}`);
   }
 }
-
 module.exports = JestFileReporter;
