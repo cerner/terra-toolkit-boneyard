2292709273fe591260ecbc362a250da4d63b707c
Sireesha Nekkanti <nekkanti.sirisha1@gmail.com>
2020-04-16 09:21:07 -0600

    added stared and endDate to the testReport

--- src/jest/JestFileReporter.js
+++ src/jest/JestFileReporter.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
 const VerboseReporter = require('@jest/reporters/build/verbose_reporter').default;
 
 const fs = require('fs');
+
+// eslint-disable-next-line import/no-unresolved
+const moment = require('moment');
 // const path = require('path');
 
 // const filePath = path.resolve(__dirname, './result.txt');
@@ -11,10 +16,23 @@ class JestFileReporter extends VerboseReporter {
     super(globalConfig);
   }
 
-  // eslint-disable-next-line class-methods-use-this
+  //  eslint-disable-next-line class-methods-use-this
+  onRunStart(aggregatedResults, options) {
+    const startDate = moment(aggregatedResults.startTime).format('MM/DD/YYYY HH:mm:ss');
+    fs.appendFile('./result.txt', `Started: ${startDate}\n\n`);
+  }
+
+  //  eslint-disable-next-line class-methods-use-this
   log(message) {
-    console.log('****', message);
-    fs.appendFile('./result.txt', `${message}\n`);
+    fs.appendFile('./result.txt', `${message}\n`, (err) => {
+      if (err) throw err;
+    });
+  }
+
+  //  eslint-disable-next-line class-methods-use-this
+  onRunComplete(_contexts, _aggregatedResults) {
+    const date = moment(Date.now()).format('MM/DD/YYYY HH:mm:ss');
+    fs.appendFile('./result.txt', `EndDate: ${date}\n\n`);
   }
 }
 module.exports = JestFileReporter;
