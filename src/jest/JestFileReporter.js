cbce5b107bfe7cc15d546b096203634dae6d24dc
Sireesha Nekkanti <nekkanti.sirisha1@gmail.com>
2020-04-23 10:46:56 -0600

    modified filepath var

--- src/jest/JestFileReporter.js
+++ src/jest/JestFileReporter.js
@@ -7,13 +7,13 @@ class JestFileReporter extends VerboseReporter {
   // eslint-disable-next-line no-useless-constructor
   constructor(globalConfig) {
     super(globalConfig);
-    this.filePath = path.resolve(globalConfig.coverageDirectory, 'JestTestResults.txt');
+    this.filePathLocation = path.resolve(globalConfig.coverageDirectory, 'JestTestResults.txt');
   }
 
   //  This stage is before the test run starts. So grab the start date/time of when the tests start.
   onRunStart(aggregatedResults) {
     const date = new Date(aggregatedResults.startTime).toLocaleString();
-    fs.appendFile(this.filePath, `Started: ${date}\n\n`, { flag: 'a+' }, (err) => {
+    fs.appendFile(this.filePathLocation, `Started: ${date}\n\n`, { flag: 'a+' }, (err) => {
       if (err) {
         console.log(`File Error -> ${err.message}`);
       }
@@ -22,7 +22,7 @@ class JestFileReporter extends VerboseReporter {
 
   // Override the verbose reporter log method such that the test results gets displayed on a text file.
   log(message) {
-    fs.appendFile(this.filePath, `${stripAnsi(message)}\n`, { flag: 'a+' }, (err) => {
+    fs.appendFile(this.filePathLocation, `${stripAnsi(message)}\n`, { flag: 'a+' }, (err) => {
       if (err) {
         console.log(`File Error -> ${err.message}`);
       }
@@ -32,7 +32,7 @@ class JestFileReporter extends VerboseReporter {
   // This stage is fired off after the log method is executed.So grab the end date/time of when the tests ended in this stage.
   onRunComplete() {
     const date = new Date().toLocaleString();
-    fs.appendFile(this.filePath, `\nEndDate: ${date}\n\n`, { flag: 'a+' }, (err) => {
+    fs.appendFile(this.filePathLocation, `\nEndDate: ${date}\n\n`, { flag: 'a+' }, (err) => {
       if (err) {
         console.log(`File Error -> ${err.message}`);
       }
